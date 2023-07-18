d25dc626b954eb4c567374aa1dd17c8acc8c4a70
Divakar <132250823+divakargmx@users.noreply.github.com>
2023-07-18 13:13:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
-from flask import Flask, request, jsonify
+import os
 import cv2
+from flask import Flask, render_template, request, jsonify
 from deepface import DeepFace
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Return the index.html file
-    return open('index.html').read()
+    return render_template('index.html')
 
 @app.route('/analyze', methods=['POST'])
 def analyze_emotion():
     data = request.json
-    image_src = data['imageSrc']
+    image_data = data['imageData'].split(",")[1]
+    
+    # Save the uploaded image locally
+    image_path = 'uploaded_image.jpg'
+    with open(image_path, 'wb') as f:
+        f.write(image_data.decode('base64'))
 
-    # Perform FER on the image
-    # Code for FER (similar to the previous example)
-
-    # For demonstration purposes, we'll return a random emotion as the result
-    # Replace this with the actual FER code
-    emotions = ['happy', 'sad', 'angry', 'neutral']
-    top_emotion = random.choice(emotions)
-
-    return jsonify({'top_emotion': top_emotion})
+    # Perform FER on the image using deepface
+    try:
+        emotion_predictions = DeepFace.analyze(image_path, actions=['emotion'])
+        emotions = emotion_predictions['emotion']
+        top_emotion = max(emotions, key=emotions.get)
+        return jsonify({'top_emotion': top_emotion})
+    except Exception as e:
+        return jsonify({'error': 'Error analyzing emotion.'})
 
 if __name__ == "__main__":
-    app.run()
+    # Run the Flask app locally using Pywebview
+    import webview
+    webview.create_window("Emotion Detector", app, width=600, height=400)
+    webview.start()
